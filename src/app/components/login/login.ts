b99525dc3c97f0ba8ae7257f1d44149443cd0c47
mosesfrancis-source <moses.francis@ndsu.edu>
2026-04-30 01:41:30 -0500

    Improved login with loading and error handling

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  private authService = inject(AuthService);
+
+  email: string = '';
+  password: string = '';
+
+  loading: boolean = false;
+  error: string = '';
+
+  onLogin(): void {
+    this.loading = true;
+    this.error = '';
+
+    // simulate backend for now
+    setTimeout(() => {
+      this.loading = false;
+
+      if (this.email === 'admin@test.com' && this.password === '1234') {
+        console.log('Login success');
+      } else {
+        this.error = 'Invalid email or password';
+      }
+    }, 1000);
+  }
+}
