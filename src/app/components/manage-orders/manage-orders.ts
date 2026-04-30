cd7bdc5c44d0fc415a4931c21fc97eee56a8f4bc
mosesfrancis-source <moses.francis@ndsu.edu>
2026-04-30 15:20:18 -0500

    Built manage orders page UI

--- src/app/components/manage-orders/manage-orders.ts
+++ src/app/components/manage-orders/manage-orders.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-manage-orders',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './manage-orders.html',
   styleUrl: './manage-orders.css',
 })
-export class ManageOrders {}
+export class ManageOrders {
+  orders = [
+    {
+      id: 1,
+      customer: 'Moses Francis',
+      items: 'Vanilla x2, Chocolate x1',
+      pickup: '3:00 PM',
+      status: 'Pending',
+    },
+    {
+      id: 2,
+      customer: 'Stephanie Pelham',
+      items: 'Strawberry x1',
+      pickup: '4:30 PM',
+      status: 'Completed',
+    },
+  ];
+
+  markComplete(order: any) {
+    order.status = 'Completed';
+  }
+
+  cancelOrder(order: any) {
+    order.status = 'Cancelled';
+  }
+}
