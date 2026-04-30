4503dbb11082bee3627333bee11896bea8dc9e71
mosesfrancis-source <moses.francis@ndsu.edu>
2026-04-30 15:05:21 -0500

    Added quantity and availability tracking to menu management

--- src/app/components/manage-menu/manage-menu.ts
+++ src/app/components/manage-menu/manage-menu.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-manage-menu',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './manage-menu.html',
   styleUrl: './manage-menu.css',
 })
