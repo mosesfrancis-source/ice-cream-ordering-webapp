aeefc3df0c5188a9eff2a0e74bb4216513833899
mosesfrancis-source <moses.francis@ndsu.edu>
2026-04-30 01:08:48 -0500

    Built login and register UI placeholders

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -3,4 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class Auth {}
+export class Auth {
+  login(email: string, password: string) {
+    console.log('Send login request to Django later', email, password);
+  }
+
+  register(name: string, email: string, password: string) {
+    console.log('Send register request to Django later', name, email, password);
+  }
+
+  logout() {
+    console.log('Logout later');
+  }
+}
